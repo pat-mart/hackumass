03e397894f41b2e41c1d5d36b190a26073b72c57
helloworld-10 <rajitj10@gmail.com>
2024-11-10 01:37:26 -0800

    Added cubic interpolation for smooth transition between light states

--- Arduino/LEDStrip/LEDStrip.ino
+++ Arduino/LEDStrip/LEDStrip.ino
@@ -9,6 +9,16 @@ CRGB leds[numLeds];
 
 //Command list: cmdType, index1, index2, r1, g1, b1
 int cmd[6];
+int s[3];
+int delta[3];
+int rgb[] = {0,0,0};
+
+float dt;
+float elapsed;
+float pt = 0.0;
+
+void cubicInterpolate(float dt);
+
 void setup() {
   Serial.begin(9600);
   FastLED.addLeds<WS2812, dataPin, GRB>(leds, numLeds); 
@@ -16,6 +26,8 @@ void setup() {
 }
 
 void loop() {
+   dt = millis()/1000.0 - pt;
+   pt = millis()/1000.0;
   if(Serial.available() > 0){
     msg = Serial.readStringUntil('\n');
     String word = "";
@@ -29,19 +41,38 @@ void loop() {
         counter++;
       }  
     }
+    cmd[counter] = word.toInt();
     if(cmd[0] == 0){
       Serial.println("connected");
     }
     else if(cmd[0] == 1){
-      for(int i = cmd[1]; i < cmd[2]; i++){
-        leds[i] = CRGB(cmd[3], cmd[4], cmd[5]);
-      }
+        delta[0] = cmd[3]-rgb[0];
+        delta[1] = cmd[4]-rgb[1];
+        delta[2] = cmd[5]-rgb[2];
+        s[0] = rgb[0];
+        s[1] = rgb[1];
+        s[2] = rgb[2];
+        elapsed = 0;
     }
     Serial.flush();
   }
-
-    
+  cubicInterpolate(dt);
+  for(int i = 0;i<numLeds;i++){
+    leds[i] = CRGB(rgb[0], rgb[1], rgb[2]);
+  }
   FastLED.show();
   delay(50);
+  
+}
+
+void cubicInterpolate(float dt){
+  if(elapsed>5.0){
+  elapsed = 5.0;
+  }
+  elapsed += dt;
+  float factor = 1.0-pow(1.0-(abs(elapsed)/5.0),3);
+  rgb[0] = (int)(s[0]+delta[0]*factor);
+  rgb[1] = (int)(s[1]+delta[1]*factor);
+  rgb[2] = (int)(s[2]+delta[2]*factor);   
   
 }
